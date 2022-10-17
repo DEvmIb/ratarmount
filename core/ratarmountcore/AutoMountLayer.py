ab88bef00beb9937d4f1d9523338c443e6181c35
DEvmIb <DEvmIb@users.noreply.github.com>
2022-10-17 18:57:03 +0200

    Update AutoMountLayer.py

--- core/ratarmountcore/AutoMountLayer.py
+++ core/ratarmountcore/AutoMountLayer.py
@@ -160,10 +160,9 @@ class AutoMountLayer(MountSource):
             _, deepestMountSource, deepestFileInfo = parentMountSource.getMountSource(archiveFileInfo)
             if isinstance(deepestMountSource, FolderMountSource):
                 # Open from file path on host file system in order to write out TAR index files.
-                #lsf
-                return None
                 mountSource = openMountSource(deepestMountSource.getFilePath(deepestFileInfo), **options)
             else:
+                return None
                 # This will fail with StenciledFile objects as returned by SQLiteIndexedTar mount sources and when
                 # given to backends like indexed_zstd, which do expect the file object to have a valid fileno.
                 mountSource = openMountSource(
